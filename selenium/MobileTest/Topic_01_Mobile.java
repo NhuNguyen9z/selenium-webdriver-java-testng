03fd58716e9e55db2b7407d5f30feb85d35e40d8
Nhu Nguyen <nhunguyen.cnnt@gmail.com>
2022-09-06 04:41:44 +0700

    Remove chromedriver.exe in Build path

--- selenium/MobileTest/Topic_01_Mobile.java
+++ selenium/MobileTest/Topic_01_Mobile.java
@@ -1,34 +1,23 @@
 package MobileTest;
 
 import org.testng.annotations.Test;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.BeforeClass;
-import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.AfterSuite;
 
 public class Topic_01_Mobile {
-	
-	
-	
-  @Test(groups = {"mobile", "regession"})
-  public void TC_01_Get_Certification() {
-	  	  
-  }
-  
-  @Test(groups = {"mobile", "regession"})
-  public void TC_02_Update_Certification() {
-  }
-  
-  @Test(groups = {"mobile", "regession"})
-  public void TC_03_Delete_Certification() {
-  }
-  
-  
+
+	@Test(groups = { "mobile", "regession" })
+	public void TC_01_Get_Certification() {
+
+	}
+
+	@Test(groups = { "mobile", "regession" })
+	public void TC_02_Update_Certification() {
+	}
+
+	@Test(groups = { "mobile", "regession" })
+	public void TC_03_Delete_Certification() {
+	}
+
+	public void TC_04_Delete_All_Certification() {
+	}
 
 }
